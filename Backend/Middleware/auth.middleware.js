33b66f467095ea968bc3202c1a2a18af6af66f63
Maxwell Ramos <maxwellramos47@gmail.com>
2026-05-29 14:35:21 -0400

    Fix Building

--- Backend/Middleware/auth.middleware.js
+++ Backend/Middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('./security.util');
-const prisma = require('../../prisma');
+const prisma = require('../Config/prisma');
 
 async function verificarToken(req, res, next) {
     const authHeader = req.headers['authorization'];
